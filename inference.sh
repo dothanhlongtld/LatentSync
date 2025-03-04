0ee1c7fa3eeac08407ab6bc535299abda3ac89d7
Long Do <dothanhlongtld@gmail.com>
2025-03-04 17:07:25 +0700

    update

--- inference.sh
+++ inference.sh
@@ -5,6 +5,6 @@ python -m scripts.inference \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
     --inference_steps 20 \
     --guidance_scale 1.5 \
-    --video_path "assets/demo1_video.mp4" \
-    --audio_path "assets/demo1_audio.wav" \
+    --video_path "assets/demo4_video.mp4" \
+    --audio_path "assets/demo4_audio.mp3" \
     --video_out_path "video_out.mp4"
