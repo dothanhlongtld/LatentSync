e2bdb517e70ee0b9bf2b3131e1d233599759a35b
Long Do <dothanhlongtld@gmail.com>
2025-04-14 16:44:50 +0700

    update

--- gradio_app_prod.py
+++ gradio_app_prod.py
@@ -95,19 +95,9 @@ def create_args(
 
 
 # Create Gradio interface
-with gr.Blocks(title="LatentSync demo") as demo:
+with gr.Blocks(title="Lipsync") as demo:
     gr.Markdown(
         """
-    <h1 align="center">LatentSync</h1>
-
-    <div style="display:flex;justify-content:center;column-gap:4px;">
-        <a href="https://github.com/bytedance/LatentSync">
-            <img src='https://img.shields.io/badge/GitHub-Repo-blue'>
-        </a> 
-        <a href="https://arxiv.org/abs/2412.09262">
-            <img src='https://img.shields.io/badge/arXiv-Paper-red'>
-        </a>
-    </div>
     """
     )
 
